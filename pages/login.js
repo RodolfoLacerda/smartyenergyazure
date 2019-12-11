53bc540c6fa03d0a19ddf3be5025845c55507517
rodolfo.lacerda <rodolfo.lacerda@memt.com.br>
2019-12-11 09:43:49 -0300

    teste

---
+++ pages/login.js
@@ -0,0 +1,16 @@
+
+module.exports = {
+    url: 'http://172.16.9.10:3000/',
+    elements: {
+        menu: '.MuiIconButton-edgeStart',
+        acessarMenu: '.MuiIconButton-colorSecondary',
+        menuContratos: '.MuiTypography-root:nth-child(3) .MuiTypography-root',
+        adicionarContrato: '.MuiButton-sizeSmall',
+        pontoConexao: '#id_connectionpoint',
+        tipoContrato: '.MuiOutlinedInput-root',
+        
+        selecionarTIpoContrato: '//li[contains(.,"Distribuição")]'
+        
+      
+    }
+}
